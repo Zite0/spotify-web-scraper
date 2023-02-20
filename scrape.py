e6383aee16f68de97864d9d7b4b4a066143e5cb2
Hannah Price <89949050+novembersvisions@users.noreply.github.com>
2023-02-19 21:04:26 -0500

    implement remove_dups

--- scrape.py
+++ scrape.py
@@ -3,13 +3,44 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id='936004f3ef804d04b78af09dbbcd8357',client_secret='56f682b2a40b4bb1b645cd2030271a49'))
 
-uri = 'spotify:artist:7Ln80lUS6He07XvHI8qqHH'
+def albums(url):
+    """
+    Returns the album names of a certain artist, given the artist's URL
+    Parameter url: URL of artist, given as a string
+    Preconditions: must be a valid URL, must be a string
+    """
+    assert isinstance(url, str)
+    assert 'https://open.spotify.com/artist/' in url
 
-results = spotify.artist_albums(uri, album_type='album')
-albums = results['items']
-while results['next']:
-    results = spotify.next(results)
-    albums.extend(results['items'])
+    # convert URL to URI
+    length = len('https://open.spotify.com/artist/')
+    uri = url[length:]
 
-for album in albums:
-    print(album['name'])
+    results = spotify.artist_albums(uri, album_type='album')
+    albums = results['items']
+
+    # check for more results
+    while results['next']:
+        results = spotify.next(results)
+        albums.extend(results['items'])
+
+    for album in albums:
+        albumlist = album['name']
+
+    return remove_dups(albumlist)
+
+def remove_dups(lst):
+    """
+    Returns a copy of the list with all occurrences of duplicate removed 
+    (except for first instance)
+    Parameter lst: must be a non-nested list
+    """
+    assert isinstance(lst, list)
+
+    copylist = []
+    for item in lst:
+        if item not in copylist:
+            copylist.append(item)
+
+    return copylist
+    
---
+++ scrapetest.py
@@ -0,0 +1,24 @@
+import scrape
+from introcs import assert_equals
+
+# Test remove_dups
+
+lst1 = ['a','b','c','d','a']
+lst2 = ['a','b','c','d']
+assert_equals(lst2,scrape.remove_dups(lst1))
+
+lst1 = ['b','b','b','b']
+lst2 = ['b']
+assert_equals(lst2,scrape.remove_dups(lst1))
+
+lst1 = ['aab','ttt','aab','aab']
+lst2 = ['aab','ttt']
+assert_equals(lst2,scrape.remove_dups(lst1))
+
+lst1 = [1,2,3,4,44,44]
+lst2 = [1,2,3,4,44]
+assert_equals(lst2,scrape.remove_dups(lst1))
+
+lst1 = [1,1,1,1]
+lst2 = [1]
+assert_equals(lst2,scrape.remove_dups(lst1))
