68b80c33baecfe019554e3108ce5f1b2b69f4017
michelle-mrz <michelle.ren00@gmail.com>
2023-03-02 14:32:55 -0500

    fixed input for coder number and added methods to class artist

--- artist.py
+++ artist.py
@@ -8,14 +8,20 @@ class Artist:
     """ 
     Artist class 
 
-    json: json dictionary from spotify API 
-    name: artist name 
-    id: aritst id 
-    album: dictionary of the form {(album, year) : [tracks]} with all albums from artist and 
+    Represents an individual artist, with the following following attributes:
+    - self.json: json dictionary extracted from spotify API 
+    - self.name: artist name 
+    - self.id: aritst id 
+    - self.album: dictionary of the form {(album, year) : [tracks]} with all albums from artist and 
     all songs associated w that album
 
     """
     def __init__(self, json):
+        """
+        json: a json dictionary 
+
+        Initializes class Artist. 
+        """
         self.json = json 
         self.name = json["name"]
         self.id = json["id"]
@@ -39,6 +45,34 @@ class Artist:
                 track_names = list(set(track_names)) # remove dupes 
                 album_dict[name,year] = track_names 
         return album_dict
+    
+    def get_album_year(self, album_name):
+        """
+        Returns album's release year given its name.
+        Returns if album is not found (invalud album name)
+
+        Parameter album_name: of type string, it's the key of the album you're looking for
+        """
+        albums = self.albums 
+        for (name, year) in albums.keys():
+            if name == album_name:
+                return year 
+            
+        return 0
+    
+    def get_album_songs(self, album_name):
+        """
+        Returns list of album's songs year given its name.
+        Returns empty list if album is not found (invalud album name)
+
+        Parameter album_name: of type string, it's the key of the album you're looking for
+        """
+        albums = self.albums 
+        for album_info in albums.keys():
+            if album_info[0] == album_name:
+                return albums[album_info]
+        
+        return []
 
     def printing(self):
         """
@@ -47,5 +81,7 @@ class Artist:
         print(f"Artist name: {self.name}")
         for album_name in self.albums.keys():
             print(f"Album name: {album_name[0]}")
-            print(f"Year released: {album_name[1]}")
-            print(f"Album {album_name[0]} tracks: {self.albums[album_name]}\n")
+            # print(f"Year released: {album_name[1]}")
+            print(f"Year released: {self.get_album_year(album_name[0])}")
+            # print(f"Album {album_name[0]} tracks: {self.albums[album_name]}\n")
+            print(f"Album {album_name[0]} tracks: {self.get_album_songs(album_name[0])}\n")
--- main.py
+++ main.py
@@ -6,39 +6,53 @@ from toCsv import *
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=CLIENT_ID,client_secret=CLIENT_SECRET))
 
-artists_list = []
-coder_number = 0
+# artists_list = []
+# coder_number = 0
 
-while 1:
-    coder_number = input("Input your coder number. ")
-    if type(int(coder_number)) != int:
-        print("Invalid coder number. Please try again. ")
-        continue 
-    coder_number = int(coder_number)
-    user_input = input("Please write artist name. Type 'QUIT' to exit program.\n")
-    if user_input.upper() == "QUIT":
-        break
-    if user_input.upper() == "" or user_input.isspace():
-        print("Invalid artist. Please try again.")
-        continue
-    result = sp.search(q = user_input, type = "artist")
-    print(result)
-    artists = result["artists"]["items"]
-    if len(artists) == 0:
-        print("Invalid artist. Please try again.")
-        continue
-    selection = input(f"Did you mean: {artists[0]['name']}? Type 'Yes' or 'No'.\n")
-    while selection.upper() not in ['YES', 'NO']:
+def main():
+    artists_list = []
+    coder_number = 0
+
+    while 1:
+        coder_number = input("Input your coder number.\n")
+        if not coder_number.isnumeric():
+            print("Invalid coder number. Please try again.\n")
+            continue 
+        coder_number = int(coder_number)
+        selection = input(f"Did you mean number {coder_number}? Type 'Yes' or 'No'.\n")
+        while selection.upper() not in ['YES', 'NO']:
+            selection = input(f"Did you mean number {coder_number}? Type 'Yes' or 'No'.\n")
+        if (selection.upper() == 'YES'):
+            break 
+        else:
+            continue 
+
+    while 1:
+        user_input = input("Please write artist name. Type 'QUIT' to exit program.\n")
+        if user_input.upper() == "QUIT":
+            break
+        if user_input.upper() == "" or user_input.isspace():
+            print("Invalid artist. Please try again.")
+            continue
+        result = sp.search(q = user_input, type = "artist")
+        # print(result) # -> print this to view json string 
+        artists = result["artists"]["items"]
+        if len(artists) == 0:
+            print("Invalid artist. Please try again.")
+            continue
         selection = input(f"Did you mean: {artists[0]['name']}? Type 'Yes' or 'No'.\n")
-    if (selection.upper() == 'YES'):
-        artist = Artist(artists[0])
-        artists_list.append(artist)
-    else:
-        continue
+        while selection.upper() not in ['YES', 'NO']:
+            selection = input(f"Did you mean: {artists[0]['name']}? Type 'Yes' or 'No'.\n")
+        if (selection.upper() == 'YES'):
+            artist = Artist(artists[0])
+            artists_list.append(artist)
+        else:
+            continue
 
+    print(f"Coder number: {coder_number}\n")
+    for artist in artists_list:
+            artist.printing()
+            # spotify_csv(artist, coder_number)
 
-for artist in artists_list:
-        spotify_csv(artist, coder_number)
-
-# if __name__ == '__main__':
-# 	main()
+if __name__ == '__main__':
+	main()
