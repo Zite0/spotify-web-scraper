891df9066717fdb470519e396775229e913a9f9e
Hannah Price <89949050+novembersvisions@users.noreply.github.com>
2023-03-20 10:47:12 -0400

    update scrapetest

--- scrapetest.py
+++ scrapetest.py
@@ -1,10 +1,3 @@
 import toCsv
-from introcs import assert_equals
-import artist
-from pandas import DataFrame as df
-import spotipy
-import artist
-from spotipy.oauth2 import SpotifyClientCredentials
-from credentials import CLIENT_SECRET, CLIENT_ID
 
-toCsv.spotify_csv('Arctic Monkeys',coder_number = 4)
+toCsv.spotify_csv('Soda Stereo',coder_number = 4)
