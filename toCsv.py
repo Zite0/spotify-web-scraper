14dc9d62e17db0802f063962415033394f587f84
Hannah Price <89949050+novembersvisions@users.noreply.github.com>
2023-03-20 09:31:45 -0400

    clean up code

--- toCsv.py
+++ toCsv.py
@@ -4,73 +4,6 @@ from artist import Artist
 from spotipy.oauth2 import SpotifyClientCredentials
 from credentials import CLIENT_SECRET, CLIENT_ID
 
-spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=CLIENT_ID,client_secret=CLIENT_SECRET))
-
-def uri_to_url(url):
-    """
-    Returns the URI of an artist, given the artist's URL
-    
-    Parameter url: URL of artist, given as a string
-    Preconditions: must be a valid URL, must be a string
-    """
-    assert isinstance(url, str)
-    assert 'https://open.spotify.com/artist/' in url
-
-    # convert URL to URI
-    length = len('https://open.spotify.com/artist/')
-    uri = url[length:]
-
-    return uri
-
-def albums(url):
-    """
-    Returns the album names of a certain artist as a dictionary, given the artist's URL.
-    Keys are IDs, values are album names.
-
-    Parameter url: URL of artist, given as a string
-    Preconditions: must be a valid URL, must be a string
-    """
-    uri = uri_to_url(url)
-
-    results = spotify.artist_albums(uri, album_type='album')
-    albums = results['items']
-
-    # check for more results
-    while results['next']:
-        results = spotify.next(results)
-        albums.extend(results['items'])
-
-    # add album names to dictionary with IDs
-    album_dict = {}
-    for album in albums:
-        album_dict[album['id']] = album['name']
-
-    remove_dict_dups(album_dict)
-
-    return(album_dict)
-
-def remove_dict_dups(d):
-    """
-    Modifies the dictionary to remove all occurrences of duplicate 
-    (except for first instance)
-    Parameter d: must be a dictionary
-    """
-    assert isinstance(d, dict)
-
-    new_dict = {}
-    values = []
-    for item in d:
-        if d[item] not in values:
-            values.append(d[item])
-            new_dict[item] = d[item]
-
-    # modify dictionary
-    d.clear()
-    for val in new_dict:
-        d[val] = new_dict[val]
-
-
-
 def _songInfo(artist,coder_number =0):
     """
     Returns a 2d list of lists of all the songs by a given artist with the following
@@ -105,9 +38,6 @@ def _songInfo(artist,coder_number =0):
     
 
     return songInfo
-
-
-        
 
 
 def spotify_csv(artist, coder_number=0):
