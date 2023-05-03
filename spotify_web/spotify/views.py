35cf44cfa4b70d3c4ba3531a0029da2a69adc32e
Hannah Price <89949050+novembersvisions@users.noreply.github.com>
2023-05-02 22:40:13 -0400

    form validation

--- spotify_web/spotify/views.py
+++ spotify_web/spotify/views.py
@@ -3,17 +3,50 @@ from django.http import HttpResponse, request
 
 def index(request):
     if request.POST:
-        context = {
-        'display_form': 'hidden',
-        'display_confirm': '',
-        'number': request.POST['number'],
-        'artist': request.POST['artist'],
-        }
-        print(request.POST['number'])
-        print(request.POST['artist'])
+        number = request.POST['number'].strip()
+        artist = request.POST['artist'].strip()
+
+        if number == '':
+            context = {
+            'number_feedback': '',
+            'artist_feedback': 'hidden',
+            'sticky_number': number,
+            'sticky_artist': artist,
+            'display_form': '',
+            'display_confirm': 'hidden',
+            }
+
+        if artist == '':
+            context = {
+            'number_feedback': 'hidden',
+            'artist_feedback': '',
+            'sticky_number': number,
+            'sticky_artist': artist,
+            'display_form': '',
+            'display_confirm': 'hidden',
+            }
+
+        if number == '' and artist == '':
+            context = {
+            'number_feedback': '',
+            'artist_feedback': '',
+            'display_form': '',
+            'display_confirm': 'hidden',
+            }
+
+        if number != '' and artist != '':
+            context = {
+            'display_form': 'hidden',
+            'display_confirm': '',
+            'number': request.POST['number'],
+            'artist': request.POST['artist'],
+            }
+
     else:
         context = {
         'display_form': '',
         'display_confirm': 'hidden',
+        'number_feedback': 'hidden',
+        'artist_feedback': 'hidden',
         }
     return render(request, 'index.html', context)
